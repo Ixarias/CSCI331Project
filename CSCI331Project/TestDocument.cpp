99b9c7f63c55d75e4d409a0db8c1e4061596a4ed
Ixarias <lacareo@gmail.com>
2019-12-13 11:43:02 -0600

    Fix minor error

--- CSCI331Project/TestDocument.cpp
+++ CSCI331Project/TestDocument.cpp
@@ -16,7 +16,7 @@ void menu(uint8_t &);
 
 int main()
 {
-	
+
 	uint8_t menuSelection = 9;	// allocates the memory for menu selection, and innitile to 0 to display menu
 
 	cout << "\n\nWelcome to CSCI 331 SS Class Program: " << endl;	// welcom message
@@ -40,11 +40,11 @@ int main()
 			cout << "*\t7. \tVerify \t\t\t\t\t*" << endl;
 			cout << "*\t8. \tRun Test Sequence\t\t\t\t\t*" << endl;
 			cout << "*\t9. \tSearch state \t\t\t\t\t*" << endl;
-			cout << "*\t10. \tQuit\t\t\t\t\t*" << endl;			
+			cout << "*\t10. \tQuit\t\t\t\t\t*" << endl;
 			cout << "*********************************************************" << endl;
 			cout << endl << endl;
 		}
-		
+
 		menu(menuSelection);	// Calls menu function and passes menu selection
 
 	}while(menuSelection != 10 );	// if selection is 10 quit the program
@@ -61,27 +61,27 @@ void menu(uint8_t &menuSelection)
 	char direction;
 	char state;
 	string zipCode;
-	vector rrnVector;
+	vector<int> rrnVector;
 
-	cout << "Menu Selection: ";		// message to user	
+	cout << "Menu Selection: ";		// message to user
 	cin >> menuSelection;			// takes in user input for menuSelection
-	cout << endl;					
+	cout << endl;
 
 	switch(menuSelection)
 	{
-		case 0: 
+		case 0:
 				break;
 
 		case 1: cout << "Enter a file name: ";
 				cin >> file_name; cout << endl;
 				//sequence.openFile(file_name); // TODO should param be a string or char* []?
 				break;
-		
+
 		case 2: cout << "Insert: ";
 				//cin >> temp; cout << endl;
 				//list.insert();	// TODO add param(s)
 				break;
-		
+
 		case 3: cout << "remove: ";
 				//cin >> temp; cout << endl;
 				//list.remove();	// TODO add param(s)
@@ -95,7 +95,7 @@ void menu(uint8_t &menuSelection)
 
 		case 6: // TODO call Display feild in recrod function
 			cout << "Display field in record\n";
-			cout << "What is the zip code you would like to know the state and place name of?:"
+			cout << "What is the zip code you would like to know the state and place name of?:";
 				cin << zipCode; cout << endl;
 			rrnVector = sequence.search(zipCode, 1);
 			for (int i = 0; i < rrnVector.size(); i++) {
@@ -130,7 +130,7 @@ void menu(uint8_t &menuSelection)
 		case 10: // quit
 				break;
 
-		default: cout << "****Please make a valid menu selection. ****" << endl << endl; 
+		default: cout << "****Please make a valid menu selection. ****" << endl << endl;
 				break;
 	}
 }
